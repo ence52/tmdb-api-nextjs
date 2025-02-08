8ada70bc7e52b6f53e092e85a973fbe4c704ee80
Erkan Ince <ince52erkan@gmail.com>
2025-02-08 23:00:53 +0300

    constants fixed

--- src/lib/api/Constants.ts
+++ src/lib/api/Constants.ts
@@ -1,5 +1,4 @@
 export const API_URL = "https://api.themoviedb.org/3";
 
 export const API_PHOTO_URL = "https://www.themoviedb.org/t/p/w780";
-export const API_ORIGINAL_PHOTO_URL =
-  " https://www.themoviedb.org/t/p/original";
+export const API_ORIGINAL_PHOTO_URL = "https://www.themoviedb.org/t/p/original";
