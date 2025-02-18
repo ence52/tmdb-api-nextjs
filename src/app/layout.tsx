1cee44f686bc41662515a229e4cce81e5a280b93
Erkan Ince <ince52erkan@gmail.com>
2025-02-18 18:48:15 +0300

    vercel analytics added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 import LayoutBody from "@/components/LayoutBody";
+import { Analytics } from "@vercel/analytics/next";
 
 export const metadata: Metadata = {
   title: "FilmScope",
@@ -33,6 +34,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <LayoutBody>{children}</LayoutBody>
+      <Analytics />
     </html>
   );
 }
